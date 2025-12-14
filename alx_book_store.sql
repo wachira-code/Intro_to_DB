60ab35bcd4c7e5e3a1d36a2fcedea2b04d864a53
Mofat Wachira <mofatwachira01@gmail.com>
2025-12-14 17:20:36 +0300

    Initial commit for book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -34,6 +34,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
